7a4bd076e9125fcad9b849fea93e1e58bde82e17
Janahan Selvanayagam <me@janahan.ca>
2025-12-26 11:54:16 +0000

    add images

--- configs/fleabottom.py
+++ configs/fleabottom.py
@@ -4,7 +4,7 @@ from itertools import product
 
 config: Dict[str, Any] = dict(
     name='fleabottom',
-    duration=100,
+    duration=60,
     size=(200,200),
     bbox=dict(width=300, height=300),
 )
@@ -18,9 +18,26 @@ config['conditions'][1] = {
     'size': (W, H),
     'colour': (255,0,0)
 }
-positions = list(product([W/4, 3*W/4], [H/4, 3*H/4])) + [(W/2, H/2)]
+
+CENTER = (W/2, H/2)
+PAD = 300
+LEFT = CENTER[0] - PAD, CENTER[1]
+RIGHT = CENTER[0] + PAD, CENTER[1]
+positions = LEFT, RIGHT
 for i, position, in enumerate(positions, start=2):
-    config['conditions'][i] = {'trial_type': 'rect', 'position': position, 'size': (W/3, H/3), 'colour': (255,0,0)}
+    config['conditions'][i] = {
+        'trial_type': 'rect', 
+        'position': position, 
+        'size': (200,200), 
+        'colour': (255,0,0)
+    }
+for i, position, in enumerate(positions, start=4):
+    config['conditions'][i] = {
+        'trial_type': 'image', 
+        'position': position, 
+        'size': (200,200), 
+        'image_path': 'stimuli/aadac.png'
+    }
 
 config['blocks'] = {}
 config['blocks'][1] = {
@@ -34,19 +51,27 @@ config['blocks'][1] = {
     ]
 }
 config['blocks'][2] = {
-    'conditions': [2, 3, 4, 5, 6],
-    'length': 100,
+    'conditions': [2, 3],
+    'length': 15,
     'method': 'random',
-    # not implemented yet but should allow for conditional transitions
+    'transition': [
+        {'condition': 'accuracy>.8', 'next': 3},
+        {'next': 1}
+    ]
+}
+config['blocks'][3] = {
+    'conditions': [2, 3, 4, 5],
+    'length': 50,
+    'method': 'random',
     'transition': [
         {'next': 1}
     ]
 }
 
-config['trial_types'] = {'rect': {
-    'module': 'trials.py',
-    'class': 'RectTrial'
-}}
+config['trial_types'] = {
+    'rect': {'module': 'trials.py', 'class': 'RectTrial'},
+    'image': {'module': 'trials.py', 'class': 'ImageTrial'},
+}
 
 def update_variables(scene: "Scene", event: "Event") -> None:
     for key, value in event['variables'].items():
